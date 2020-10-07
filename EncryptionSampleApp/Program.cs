1dbec7a2f0f339c00154e34e53c3dedefaee7afd
Erdem <erdemsari@gmail.com>
2020-10-07 14:31:29 +0300

    ini

---
+++ EncryptionSampleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EncryptionSampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var key = "32karaktersifre12345678901234567";
+                     
+
+
+            Console.WriteLine("Please enter a string for encryption");
+            var str = Console.ReadLine();
+            var encryptedString = AESEncryption.EncryptString(key, str);
+            Console.WriteLine($"encrypted string = {encryptedString}");
+
+            var decryptedString = AESEncryption.DecryptString(key, encryptedString);
+            Console.WriteLine($"decrypted string = {decryptedString}");
+
+            Console.ReadKey();
+        }
+    }
+}
